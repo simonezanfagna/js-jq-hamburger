7c1650d522adde6fd119bed9c85b47263737f16d
simonezanfagna <zanfagnasimone@live.it>
2019-11-25 18:13:01 +0100

    fine

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.header-right > a').click(function () {
+  $('.hamburger-menu').addClass('active');
+});
+
+$('.hamburger-menu .close').click(function () {
+  $('.hamburger-menu').removeClass('active');
+})
